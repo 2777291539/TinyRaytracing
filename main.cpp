45a8e9e62a1c4801aab3bb1462a681dd4debe0a5
dcr <974449413@qq.com>
2023-07-29 21:44:32 +0800

    半球向量生成的随机点
    Signed-off-by: dcr <974449413@qq.com>

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ vec3 ray_color(const ray& r, const hittable& world, int depth) {
         return vec3(0,0,0);
     }
     if (world.hit(r, 0.001, infinity, rec)) {
-        vec3 target = rec.p + rec.normal + random_unit_vector();
+        vec3 target = rec.p + random_in_hemisphere(rec.normal);
         return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
     vec3 unit_direction = unit_vector(r.direction());
