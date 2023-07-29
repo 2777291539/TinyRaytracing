1c2a4e2802c3a693594cfba33337573782289659
dcr <974449413@qq.com>
2023-07-29 14:00:08 +0800

    回归正途的提交
    Signed-off-by: dcr <974449413@qq.com>

--- ray.h
+++ ray.h
@@ -6,7 +6,7 @@
 class ray {
 public:
     ray() {}
-    ray(const vec3& origin, const vec3& direction) : orig(origin), dir(direction) { dir = unit_vector(dir); } 
+    ray(const vec3& origin, const vec3& direction) : orig(origin), dir(direction) {} 
 
     vec3 origin() const { return orig; }
     vec3 direction() const {return dir; }
