028b37c15612893d314380ae6d7b70edebb9de16
dcr <974449413@qq.com>
2023-07-30 15:37:24 +0800

    设置摄像机，并使用广角镜头渲染两个球
    Signed-off-by: dcr <974449413@qq.com>

--- main.cpp
+++ main.cpp
@@ -38,12 +38,15 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     hittable_list world;
-    world.add(std::make_shared<sphere>(vec3(0,0,-1), 0.5, std::make_shared<lambertian>(vec3(0.1, 0.2, 0.5))));
-    world.add(std::make_shared<sphere>(vec3(0,-100.5,-1), 100, std::make_shared<lambertian>(vec3(0.8, 0.8, 0.0))));
-    world.add(std::make_shared<sphere>(vec3(1,0,-1), 0.5, std::make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.0)));
-    world.add(std::make_shared<sphere>(vec3(-1,0,-1), 0.5, std::make_shared<dielectric>(1.5)));
-    world.add(std::make_shared<sphere>(vec3(-1,0,-1), -0.45, std::make_shared<dielectric>(1.5)));
-    camera cam;
+    //world.add(std::make_shared<sphere>(vec3(0,0,-1), 0.5, std::make_shared<lambertian>(vec3(0.1, 0.2, 0.5))));
+    //world.add(std::make_shared<sphere>(vec3(0,-100.5,-1), 100, std::make_shared<lambertian>(vec3(0.8, 0.8, 0.0))));
+    //world.add(std::make_shared<sphere>(vec3(1,0,-1), 0.5, std::make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.0)));
+    //world.add(std::make_shared<sphere>(vec3(-1,0,-1), 0.5, std::make_shared<dielectric>(1.5)));
+    //world.add(std::make_shared<sphere>(vec3(-1,0,-1), -0.45, std::make_shared<dielectric>(1.5)));
+    auto R = cos(pi/4);
+    camera cam(90, double(image_width)/image_height);
+    world.add(std::make_shared<sphere>(vec3(-R,0,-1), R, std::make_shared<lambertian>(vec3(0, 0, 1))));
+    world.add(std::make_shared<sphere>(vec3(R,0,-1), R, std::make_shared<lambertian>(vec3(1, 0, 0))));
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
