5394c88d5bef698cca568b1742e2cdc6fc838f39
dcr <974449413@qq.com>
2023-07-30 14:23:20 +0800

    添加绝缘体材质，添加折射功能，添加出透明物体
    Signed-off-by: dcr <974449413@qq.com>

--- Tool/rtweekend.h
+++ Tool/rtweekend.h
@@ -32,6 +32,12 @@ inline double clamp(double x, double min, double max) {
     return x;
 }
 
+inline double schlick(double cosine, double ref_idx) {
+    auto r0 = (1-ref_idx) / (1+ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1-r0) * pow((1-cosine), 5);
+}
+
 #include "ray.h"
 #include "vec3.h"
 
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "camera.h"
 #include "lambertian.h"
 #include "metal.h"
+#include "dielectric.h"
 
 #include <iostream>
 
@@ -37,10 +38,11 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     hittable_list world;
-    world.add(std::make_shared<sphere>(vec3(0,0,-1), 0.5, std::make_shared<lambertian>(vec3(0.7, 0.3, 0.3))));
+    world.add(std::make_shared<sphere>(vec3(0,0,-1), 0.5, std::make_shared<lambertian>(vec3(0.1, 0.2, 0.5))));
     world.add(std::make_shared<sphere>(vec3(0,-100.5,-1), 100, std::make_shared<lambertian>(vec3(0.8, 0.8, 0.0))));
-    world.add(std::make_shared<sphere>(vec3(1,0,-1), 0.5, std::make_shared<metal>(vec3(0.8, 0.6, 0.2), 1.0)));
-    world.add(std::make_shared<sphere>(vec3(-1,0,-1), 0.5, std::make_shared<metal>(vec3(0.8, 0.8, 0.8), 0.3)));
+    world.add(std::make_shared<sphere>(vec3(1,0,-1), 0.5, std::make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.0)));
+    world.add(std::make_shared<sphere>(vec3(-1,0,-1), 0.5, std::make_shared<dielectric>(1.5)));
+    world.add(std::make_shared<sphere>(vec3(-1,0,-1), -0.45, std::make_shared<dielectric>(1.5)));
     camera cam;
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
